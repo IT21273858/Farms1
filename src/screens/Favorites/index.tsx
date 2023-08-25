71d6f9625594cc324ca36761d67345c1ba439b4e
IT21273858 <IT21273858@my.sliit.lk>
2023-08-25 10:41:58 +0530

    Extra commenting to favourites

--- src/screens/Favorites/index.tsx
+++ src/screens/Favorites/index.tsx
@@ -13,32 +13,32 @@ import {
 } from './styles';
 
 import { getFavorites } from '../../utils/storage';
-
+      // Defining the 'Favorites' component
 export default function Favorites() {
-
+      // Hook to check if the screen is focused
   const isFocused = useIsFocused();
-
+      // State to store the user's favorite products
   const [favorites, setFavorites] = useState<ProductProps[] | []>([]);
 
   useEffect(() => {
-
+    // Variable tracks is the component is still active
     let isActive = true;
-
+    // Fetch favorite products from api
     async function getMyFavorites() {
       const products = await getFavorites('@appfakestore');
-      setFavorites(products);
+      setFavorites(products); // Updates the favorites state only if the component is still active
     }
-
+    // Fetch favorites only when the component is mounted and focused
     if (isActive) {
       getMyFavorites();
     }
-
+    // sets 'isActive' to false when the component is unmounted
     return () => {
       isActive = false;
     };
 
   }, [isFocused]);
-
+  // Render each individual item in the FlatList
   const renderItem = ({ item }: ListRenderItemInfo<ProductProps>) => <ProductCard {...item} />;
 
   return (
